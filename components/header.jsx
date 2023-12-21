56776e47f401c6fbe6c848a6a2e2c9fd30b2841e
fodhil-ben <benhibafodhil@gmail.com>
2023-12-21 13:48:47 +0100

    Edit in the portfolio link

--- components/header.jsx
+++ components/header.jsx
@@ -2,7 +2,7 @@ function Header() {
     return (
         <header>
             <a className="feedback" target="_blank" href="https://docs.google.com/forms/d/e/1FAIpQLScxQZBrKH_Jkc77J24UWhVlCWQGUpq_kAPeJw43uPC-ISyZrg/viewform?usp=sf_link" > Give Feedback </a>
-            <a className="about" target="_blank" href="https://fodhil-ben.github.io/Portfolio/">About Me</a>
+            <a className="about" target="_blank" href="https://fodhil-benhiba.vercel.app/">About Me</a>
         </header>
     )
 }
